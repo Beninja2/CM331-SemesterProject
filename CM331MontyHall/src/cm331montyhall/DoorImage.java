2626a7778dc789ea3d836aa09280959109e3f8fa
Aaron D. Morris <rndmorris@gmail.com>
2016-12-12 22:23:06 -0600

    Separated the UI and game logic. We now have the ability to play half of the game. All that is left is to implement showing the winning door.
    TODO: Implement AI in full.

---
+++ CM331MontyHall/src/cm331montyhall/DoorImage.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm331montyhall;
+
+import java.io.File;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author rndmorris
+ */
+public class Door extends StackPane {
+    
+    private static final String CLOSED = File.separator + "img" + File.separator + "door_closed.png";
+    private static final String LOSER = File.separator + "img" + File.separator + "door_loser.png";
+    private static final String WINNER = File.separator + "img" + File.separator + "door_winner.png";
+    private boolean prize = false;
+    private boolean selected = false;
+    private boolean opened = false;
+    private int doorId = -1;
+    private ImageView doorLayer = new ImageView(CLOSED);
+    
+    
+    public Door(boolean prize,int doorId) {
+        super();
+        setPrize(prize);
+        this.doorId = doorId;
+        this.setHeight(doorLayer.getImage().getHeight()+10);
+        this.setWidth(doorLayer.getImage().getWidth()+10);
+        this.getChildren().add(doorLayer);
+    }
+    public boolean isPrize() {
+        return prize;
+    }
+    public void setPrize(boolean prize) {
+        this.prize = prize;
+    }
+    public void openDoor() {
+        this.opened = true;
+        if (isPrize()) {
+            doorLayer.setImage(new Image(WINNER));
+        } else {
+            doorLayer.setImage(new Image(LOSER));
+        }
+    }
+    public void setSelected(boolean selected) {
+        if (selected) {
+            this.setStyle("-fx-background-color: #00ff00");
+        } else {
+            this.setStyle("-fx-background-color: #ffffff");
+        }
+        this.selected = selected;
+    }
+    public boolean isSelected() {
+        return selected;
+    }
+    public int getDoorId() {
+        return this.doorId;
+    }
+    public boolean isOpened() {
+        return opened;
+    }
+}
